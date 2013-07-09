e2b4d0c9ddd8b0e83fa488555bf4a997bbbeb0da
Nam Nguyen <nam@digital-minds.mobi>
2013-07-09 10:15:24 +0700

    Initial Commit

---
+++ FastFingerFree/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  FastFingerFreeAppController.h
+//  FastFingerFree
+//
+//  Created by Nam Nguyen on 7/9/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ FastFingerFree/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  FastFingerFreeAppController.h
+//  FastFingerFree
+//
+//  Created by Nam Nguyen on 7/9/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
